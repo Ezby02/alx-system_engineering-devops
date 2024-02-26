9c921b457726f37284bea2b110535bd684894689
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2024-02-26 22:31:30 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,10 @@
-# Installing flask from pip3
+# install 'flask' from 'pip3'
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
 
 package { 'python3-pip':
   ensure => installed,
